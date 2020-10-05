3655d6d778694c999932fd8d38189c55e7fe7813
VK <kolupaev.vladislav@gmail.com>
2020-10-05 21:45:37 +0300

    delete prints

--- main.py
+++ main.py
@@ -20,20 +20,12 @@ def print_html_test(table):
             table_dict[row_index] = {}
             table_dict[row_index]['context'] = 'table, tr'
             table_dict[row_index]['cells'] = []
-            # print(row.attrs)
             table_dict[row_index]['attrs'] = row.attrs
-            # print(table_dict[row_index]['attrs']) 
             for cell in row.children:
                 if type(cell).__name__ != 'NavigableString':
                     table_dict[row_index]['cells'].append([])
                     table_dict[row_index]['cells'][cell_index] = {}
                     table_dict[row_index]['cells'][cell_index]['attrs'] = cell.attrs
-                    # print(row_index)
-                    # print(cell_index)
-                    # print(cell.attrs)
-                    # print(cell)
-                    # print(table_dict[row_index]['cells'][cell_index]['attrs'])
-                    # print(cell.attrs)
                     values_index = 0
                     table_dict[row_index]['cells'][cell_index]['values'] = []
                     for cell_values in cell.contents:
@@ -47,13 +39,9 @@ def print_html_test(table):
                                 if cell_class:
                                     cell_class = cell_class[0] 
                                 table_dict[row_index]['cells'][cell_index]['values'][values_index]['context'] = cell_class
-                            # print(table_dict[row_index]['cells'][cell_index]['values'][values_index]['text'])
                             values_index += 1
-                            # print(cell_values.string)
                     cell_index += 1
-            # print(cell_index)
             row_index += 1
-    # print(table_dict) 
     return table_dict
 
 
@@ -78,8 +66,6 @@ def table_to_excel(table_dict, start_cell):
                 colspan = int(cell['attrs'].get('colspan', None))
             end_x = cur_x + rowspan - 1
             end_y = cur_y + colspan - 1 
-            print(f'{rowspan} {colspan}')
-            print(f'{cur_x} {cur_y} / {end_x} {end_y}')
             set_x_y = set(add_to_set(set_x_y, cur_x, cur_y, end_x, end_y)) 
             add_text = ''
             for i in cell['values']:
@@ -87,7 +73,6 @@ def table_to_excel(table_dict, start_cell):
                     add_text += i['text'] + str('\n')
                 else:
                     add_text += i['text']
-            # worksheet.write(cur_x, cur_y, add_text)
             if rowspan + colspan > 2:
                 if rowspan > 1:
                     cell_format = workbook.add_format()
@@ -104,23 +89,13 @@ def table_to_excel(table_dict, start_cell):
                 cell_format_two = workbook.add_format()
                 cell_format_two.set_border() 
                 worksheet.write(cur_x, cur_y, add_text, cell_format_two)
-            # print(cell['attrs'])
-            # print(cell['values']) 
-            # print(rowspan)
-            # print(colspan)
-            # print(f'{cur_x} {cur_y}')
-            # print(f'{end_x} {end_y}')
             cur_y = end_y
             while (cur_x, cur_y) in set_x_y:
                 cur_y += 1
-            # print(f'{cur_x} {cur_y}')
-            print(set_x_y)
         cur_y = 0
         cur_x += 1
         while (cur_x, cur_y) in set_x_y:
             cur_y += 1
-        # print('*****')
-        # print(f'{cur_x} {cur_y}')
     workbook.close()
         
 
@@ -129,10 +104,5 @@ table_dict = list(print_html_test(soup.table))
 start_cell = (0, 0)
 table_to_excel(table_dict, start_cell)
 
-for i in range(1,3):
-    print(i)
 
 
-# for child in soup.table:
-#     if type(child).__name__ != 'NavigableString':
-#         print(child.contents)
